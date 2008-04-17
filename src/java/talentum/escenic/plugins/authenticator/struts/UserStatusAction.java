661ed3e89f89932b1989843e9baa6cd888b5a50a
stefan.norman <stefan.norman@ccec6eda-18fc-447e-8b29-203d4591b618>
2008-04-17 10:26:16 +0000

    AFV-378

--- src/java/talentum/escenic/plugins/authenticator/struts/UserStatusAction.java
+++ src/java/talentum/escenic/plugins/authenticator/struts/UserStatusAction.java
@@ -8,16 +8,25 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import talentum.escenic.plugins.authenticator.AuthenticatorManager;
+import talentum.escenic.plugins.authenticator.authenticators.AuthenticatedUser;
+
 public class UserStatusAction  extends Action {
 
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		
-		if(request.getAttribute("authenticatedUser") == null) {
+		String token = request.getParameter("token");
+		AuthenticatedUser user = AuthenticatorManager.getInstance().getUser(token);
+		
+		if(user == null) {
 			response.sendError(HttpServletResponse.SC_FORBIDDEN);
 			return null;
+		} else {
+			request.setAttribute("authenticatedUser", user);
 		}
+
 		return mapping.findForward("status");
 	}
 }

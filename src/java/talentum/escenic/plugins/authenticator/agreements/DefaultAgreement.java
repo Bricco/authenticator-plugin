054834017c3e0b222c8d9cb7ac83afbe22e6401c
stefan.norman <stefan.norman@ccec6eda-18fc-447e-8b29-203d4591b618>
2013-02-19 08:51:37 +0000

    POC-856

--- src/java/talentum/escenic/plugins/authenticator/agreements/DefaultAgreement.java
+++ src/java/talentum/escenic/plugins/authenticator/agreements/DefaultAgreement.java
@@ -1,7 +1,10 @@
 package talentum.escenic.plugins.authenticator.agreements;
 
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import neo.xredsys.api.Section;
@@ -31,6 +34,10 @@ public abstract class DefaultAgreement implements AgreementPartner {
 	AgreementConfig config;
 
 	private HashMap urlMap;
+	
+	private String freemiumRole;
+	private int freemiumNoOfFreeArticles;
+	private int freemiumDaysAsLoggedIn;
 
 	/**
 	 * Constructor. It sets up the agreement configuration.
@@ -59,6 +66,30 @@ public abstract class DefaultAgreement implements AgreementPartner {
 
 	public void addUrl(String pName, String pUrl) {
 		urlMap.put(pName, pUrl);
+	}
+
+	public String getFreemiumRole() {
+		return freemiumRole;
+	}
+
+	public void setFreemiumRole(String freemiumRole) {
+		this.freemiumRole = freemiumRole;
+	}
+
+	public int getFreemiumNoOfFreeArticles() {
+		return freemiumNoOfFreeArticles;
+	}
+
+	public void setFreemiumNoOfFreeArticles(int freemiumNoOfFreeArticles) {
+		this.freemiumNoOfFreeArticles = freemiumNoOfFreeArticles;
+	}
+
+	public int getFreemiumDaysAsLoggedIn() {
+		return freemiumDaysAsLoggedIn;
+	}
+
+	public void setFreemiumDaysAsLoggedIn(int freemiumDaysAsLoggedIn) {
+		this.freemiumDaysAsLoggedIn = freemiumDaysAsLoggedIn;
 	}
 
 	/**
@@ -93,60 +124,110 @@ public abstract class DefaultAgreement implements AgreementPartner {
 			}
 		}
 
-		// get the user that was set in the filter
-		AuthenticatedUser user = (AuthenticatedUser) request
-				.getRequestAttribute("authenticatedUser");
+//		// get the user that was set in the filter
+//		AuthenticatedUser user = (AuthenticatedUser) request
+//				.getRequestAttribute("authenticatedUser");
+		
+		try {
+			URL url = new URL(request.getUrl());
+		
+			// set headers for Varnish
+		
+			// which roles are allowed
+			response.setHeader("X-Paywall-Roles", requestedRole);
+			// where should unauthorized users be redirected
+			response.setHeader("X-Paywall-Denied-Url", url.getPath() + "?showPopup=" +
+					encodeForUrl((String)urlMap.get("loginform")));
+			// where should evicted users be redirected
+			response.setHeader("X-Paywall-Rejected-Url", (String)urlMap.get("rejected"));
+			// where should users with status passive be redirected
+			response.setHeader("X-Paywall-Passive-Url", (String)urlMap.get("passive"));
+			
+			// if the configured freemium role is set and is required by the requested url,
+			// add headers for metered. Only for articles.
+//			if(context.equalsIgnoreCase("art") &&
+//					getFreemiumRole() != null &&
+//					Arrays.asList(splitCommaSeparatedString(requestedRole)).contains(getFreemiumRole())) {
+//				// allowed views 
+//				response.setHeader("X-Paywall-Metered-Allowed-Views", getFreemiumRole().trim());
+//				// set the article ID as identifier
+//				Object article = request
+//						.getRequestAttribute("com.escenic.context.article");
+//				// TODO !!!!
+//				response.setHeader("X-Paywall-Metered-Identifier", getFreemiumRole().trim());
+//				// TODO change name of header
+//				response.setHeader("X-Paywall-Denied-Url", "");
+//				// TODO implement 
+//				response.setHeader("X-Paywall-Metered-Cookie-Expires", getFreemiumRole().trim());
+//			}
 
-		if (user == null) {
-
-			String publicationName = (String) request
-					.getRequestAttribute("com.escenic.publication.name");
-
-			// get the token from the cookie and check if user has been evicted
-			String token = request.getCookie(AuthenticatorManager.getInstance()
-					.getCookieName(publicationName));
-			if (token != null
-					&& AuthenticatorManager.getInstance().userHasBeenEvicted(
-							token)) {
-				if (log.isDebugEnabled()) {
-					log.debug("User " + user
-							+ " was rejected, another user was logged in");
-				}
-				// if the user has been evicted and another user is using the
-				// account
-				response.setRedirect(getContextPath(request)
-						+ urlMap.get("rejected"));
-			} else {
-				if (log.isDebugEnabled()) {
-					log.debug("User " + user + " not found");
-				}
-				// if the user is not found redirect to login page
-				response.setRedirect(request.getUrl() + "?showPopup=" +
-						encodeForUrl((String)urlMap.get("loginform")));
-			}
-
-		} else if (requestedRole != null && !user.hasRole(splitCommaSeparatedString(requestedRole))) {
-
-			if (log.isDebugEnabled()) {
-				log.debug("User " + user + " not authorized for role(s) "
-						+ requestedRole);
-			}
-			// if user is logged in but not authorized send to unauthorized page
-			response.setRedirect(getContextPath(request)
-					+ urlMap.get("unauthorized"));
-
-		} else if (requestedRole != null
-				&& user.hasPassiveStatusForRole(splitCommaSeparatedString(requestedRole))) {
-
-			if (log.isDebugEnabled()) {
-				log.debug("User " + user + " has passive status for role "
-						+ requestedRole);
-			}
-			// if user is logged in but has status passive send to passive page
-			response.setRedirect(getContextPath(request)
-					+ urlMap.get("passive"));
-
+		} catch (MalformedURLException e) {
+			log.error("Could not create url object from request url.", e);
 		}
+		
+//		if (user == null) {
+//
+//			String publicationName = (String) request
+//					.getRequestAttribute("com.escenic.publication.name");
+//
+//			// get the token from the cookie and check if user has been evicted
+//			String token = request.getCookie(AuthenticatorManager.getInstance()
+//					.getCookieName(publicationName));
+//			if (token != null
+//					&& AuthenticatorManager.getInstance().userHasBeenEvicted(
+//							token)) {
+//				if (log.isDebugEnabled()) {
+//					log.debug("User " + user
+//							+ " was rejected, another user was logged in");
+//				}
+//				// if the user has been evicted and another user is using the
+//				// account
+//				response.setRedirect(getContextPath(request)
+//						+ urlMap.get("rejected"));
+//			} else {
+//				if (log.isDebugEnabled()) {
+//					log.debug("User " + user + " not found");
+//				}
+//				// if the user is not found redirect to login page
+//				response.setRedirect(request.getUrl() + "?showPopup=" +
+//						encodeForUrl((String)urlMap.get("loginform")));
+//			}
+//
+//		} else if (requestedRole != null
+//				&& !checkFreemium(user, requestedRole)) {
+//			
+//			if (log.isDebugEnabled()) {
+//				log.debug("User " + user + " not authorized for freemium with role(s) "
+//						+ requestedRole);
+//			}
+//			// if user is logged in but not authorized send to unauthorized page
+//			response.setRedirect(getContextPath(request)
+//					+ urlMap.get("unauthorized"));
+//			
+//			
+//		} else if (requestedRole != null
+//				&& user.matchingRoles(splitCommaSeparatedString(requestedRole)).length == 0) {
+//
+//			if (log.isDebugEnabled()) {
+//				log.debug("User " + user + " not authorized for role(s) "
+//						+ requestedRole);
+//			}
+//			// if user is logged in but not authorized send to unauthorized page
+//			response.setRedirect(getContextPath(request)
+//					+ urlMap.get("unauthorized"));
+//
+//		} else if (requestedRole != null
+//				&& user.hasPassiveStatusForRole(splitCommaSeparatedString(requestedRole))) {
+//
+//			if (log.isDebugEnabled()) {
+//				log.debug("User " + user + " has passive status for role "
+//						+ requestedRole);
+//			}
+//			// if user is logged in but has status passive send to passive page
+//			response.setRedirect(getContextPath(request)
+//					+ urlMap.get("passive"));
+//
+//		}
 
 	}
 
@@ -190,6 +271,29 @@ public abstract class DefaultAgreement implements AgreementPartner {
 		}
 		return false;
 	}
+
+	/**
+	 * Check if user is has a valid freemium role
+	 * @param user the User
+	 * @param requestedRole the requested role(s)
+	 * @return true if user has valid freemium role
+	 */
+	private boolean checkFreemium(AuthenticatedUser user, String requestedRole) {
+		// if not freemiumRole is set
+		if(getFreemiumRole() == null) {
+			return true;
+		}
+		// if the requested roles does not contain the freemium role
+		if(!Arrays.asList(splitCommaSeparatedString(requestedRole)).contains(getFreemiumRole())) {
+			return true;
+		}
+		// if the user has the freemium role
+		if(Arrays.asList(user.getRoles()).contains(getFreemiumRole())) {
+//			if(getFreemiumDaysAsLoggedIn())
+		}
+		
+		return false;
+	}
 	
 	private String getContextPath(AgreementRequest request) {
 
@@ -212,7 +316,7 @@ public abstract class DefaultAgreement implements AgreementPartner {
 	 * @param s the string to split
 	 * @return at least an empty array
 	 */
-	private String[] splitCommaSeparatedString(String s){
+	public static String[] splitCommaSeparatedString(String s){
 	    String[] arr = new String[0];
 	    if (s != null) {
 	    	if(s.indexOf(',') > 0) {
